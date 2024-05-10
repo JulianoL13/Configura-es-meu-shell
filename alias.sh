8db43de4ef070c3f29717faed54381e8b334fbf4
JulianoL13 <125844980+JulianoL13@users.noreply.github.com>
2024-05-10 18:31:26 -0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,3 +1,5 @@
+
+clear co
 echo "Segue abaixo alguns comandos que eu utilizo no meu arquivo Alias"
 echo "Relembrando que esses comandos são focados em distros baseadas em Debian. Planejo fazer uma versão para o Arch em breve"
 
@@ -23,15 +25,23 @@ echo "alias upd='sudo apt update && sudo apt upgrade'"
   
 
 #Instalar algumas dependências
-sudo apt install git
-sudo apt install exa
-sudo apt install bat
-mkdir -p ~/.local/bin
-ln -s /usr/bin/batcat ~/.local/bin/bat
+echo -e "\033[34mDeseja instalar as dependências? (s/n)"
+read resposta
+
+if [ "$resposta" != "${resposta#[Ss]}" ] ;then
+    echo "Instalando dependências..."
+    sudo apt install git
+    sudo apt install exa
+    sudo apt install bat
+else
+    echo "Pulando a instalação das dependências..."
+fi
+
 
 #configurar bash
 touch ~/alias.txt
-echo -e "# Git\nalias g='git'\nalias gck='git checkout'\nalias gst='git status'\nalias gps='git push'\nalias gpl='git pull'\nalias gcm='git commit'\n# Shell\nalias ls='exa --icons'\nalias cat='bat'\nalias find='fd'\nalias grep='rg'\nalias ps='procs'\nalias mkdir='mkdir -pv'\nalias ips='ip -c -br a'\n### Distros baseadas em debian\nalias upd='sudo apt update && sudo apt upgrade'" >> alias.txt
+chmod 777 ~/alias.txt
+echo -e "# Git\nalias g='git'\nalias gck='git checkout'\nalias gst='git status'\nalias gps='git push'\nalias gpl='git pull'\nalias gcm='git commit'\n# Shell\nalias ls='exa --icons'\nalias cat='bat'\nalias find='fd'\nalias grep='rg'\nalias ps='procs'\nalias mkdir='mkdir -pv'\nalias ips='ip -c -br a'\n### Distros baseadas em debian\nalias upd='sudo apt update && sudo apt upgrade'" >> ~/alias.txt
 
 if [ -f ~/.bashrc ]; then
     echo "source ~/alias.txt" >> ~/.bashrc
@@ -40,3 +50,5 @@ elif [ -f ~/.bash_profile ]; then
 else
     echo "Nenhum arquivo de perfil do Bash encontrado."
 fi
+
+echo "Tudo pronto. Aproveite seus novos Alias!"
