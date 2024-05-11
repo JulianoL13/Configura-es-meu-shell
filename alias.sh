22246a82b6a3386749b9164f8b3defe50c86cd9d
JulianoL13 <125844980+JulianoL13@users.noreply.github.com>
2024-05-11 17:06:07 -0300

    Update 1.0.1 alias.sh
    Organizando o menu/adicionando algumas funcionalidades

--- alias.sh
+++ alias.sh
@@ -1,8 +1,64 @@
+MENU() {
+PS3="└──> Menu : "
+options=(
+    "1 - Configurar alias"
+    "2 - Mostrar comandos alterados/criados"
+    "3 - Sair"
+)
+select opt in "${options[@]}"
+do
+    case $opt in
+        "1 - Configurar alias")
+            CONFIG_BASH
+            ;;
+        "2 - Mostrar comandos alterados/criados")
+            SHOWCMDS
+            ;;
+        "3 - Sair")
+            exit 0
+            ;;
+        *)
+            echo "Opção inválida: $REPLY"
+            ;;
+    esac
+done
 
-clear 
-echo -e "\033[34mSegue abaixo alguns comandos que eu utilizo no meu arquivo Alias"
-echo -e "\033[34mRelembrando que esses comandos são focados em distros baseadas em Debian. Planejo fazer uma versão para o Arch em breve"
+    }
 
+
+CONFIG_BASH() {
+    #Configurando dependências
+    echo -e "\031[34mDeseja instalar as dependências? (Y/n)"
+    read resposta
+
+    if [ -z "$resposta" ] || [ "$resposta" != "${resposta#[SsYy]}" ] ;then
+        echo "Instalando dependências..."
+        sudo apt install git
+        sudo apt install exa
+        sudo apt install bat
+    else
+        echo "Pulando a instalação das dependências..."
+    fi    
+
+    #Configurando o bash
+    touch ~/alias.txt
+    chmod 777 ~/alias.txt
+    echo -e "# Git\nalias g='git'\nalias gck='git checkout'\nalias gst='git status'\nalias gps='git push'\nalias gpl='git pull'\nalias gcm='git commit'\n# Shell\nalias ls='exa --icons'\nalias cat='bat'\nalias find='fd'\nalias grep='rg'\nalias ps='procs'\nalias mkdir='mkdir -pv'\nalias ips='ip -c -br a'\n### Distros baseadas em debian\nalias upd='sudo apt update && sudo apt upgrade'" >> ~/alias.txt
+
+    if [ -f ~/.bashrc ]; then
+        echo "source ~/alias.txt" >> ~/.bashrc
+    elif [ -f ~/.bash_profile ]; then
+        echo "source ~/alias.txt" >> ~/.bash_profile
+    elif [ -f ~/.zshrc ]; then
+        echo "source ~/alias.txt" >> ~/.zshrc
+    else
+        echo "Nenhum arquivo de perfil do Bash encontrado."
+    fi
+    echo "Tudo pronto. Aproveite seus novos Alias!"
+}
+
+
+SHOWCMDS() {
 echo -e "\033[34m# Git"
 echo -e "\033[34malias g='git'"
 echo -e "\033[34malias gck='git checkout'"
@@ -22,34 +78,11 @@ echo -e "\033[34malias ips='ip -c -br a'"
 
 echo -e "\033[34m### Distros baseadas em debian"
 echo -e "\033[34malias upd='sudo apt update && sudo apt upgrade'"
+}
 
-  
+###Início###
 
-#Instalar algumas dependências
-echo -e "\031[34mDeseja instalar as dependências? (s/n)"
-read resposta
-
-if [ "$resposta" != "${resposta#[Ss]}" ] ;then
-    echo "Instalando dependências..."
-    sudo apt install git
-    sudo apt install exa
-    sudo apt install bat
-else
-    echo "Pulando a instalação das dependências..."
-fi
-
-
-#configurar bash
-touch ~/alias.txt
-chmod 777 ~/alias.txt
-echo -e "# Git\nalias g='git'\nalias gck='git checkout'\nalias gst='git status'\nalias gps='git push'\nalias gpl='git pull'\nalias gcm='git commit'\n# Shell\nalias ls='exa --icons'\nalias cat='bat'\nalias find='fd'\nalias grep='rg'\nalias ps='procs'\nalias mkdir='mkdir -pv'\nalias ips='ip -c -br a'\n### Distros baseadas em debian\nalias upd='sudo apt update && sudo apt upgrade'" >> ~/alias.txt
-
-if [ -f ~/.bashrc ]; then
-    echo "source ~/alias.txt" >> ~/.bashrc
-elif [ -f ~/.bash_profile ]; then
-    echo "source ~/alias.txt" >> ~/.bash_profile
-else
-    echo "Nenhum arquivo de perfil do Bash encontrado."
-fi
-
-echo "Tudo pronto. Aproveite seus novos Alias!"
+clear 
+echo -e "\033[34mSegue abaixo um script que estou desenvolvendo para facilitar a utilização de sistemas Linux, principalmente para iniciantes"
+echo -e "\033[34mRelembrando que esses comandos são focados em distros baseadas em Debian. Planejo fazer uma versão para o Arch em breve"
+echo -e "\033[34mRepositório: https://github.com/JulianoL13/Configura-es-meu-shell.git"
